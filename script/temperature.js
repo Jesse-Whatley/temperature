99f07a21621146fbd32d32a76fe62fa0f98fd803
Jesse-Whatley <crackerculturecollections@gmail.com>
2024-03-15 21:26:57 -0400

    i couldnt make my css work properly and had to turn in assignment

--- script/temperature.js
+++ script/temperature.js
@@ -1,17 +1,20 @@
 function convertTemperatureRange(){
+    
     let startValue = prompt("Enter A Start Number:");
     let endValue = prompt("Enter A End Number:");
     let scale = prompt("Enter A Scale: 1)F 2)C");
 
     for(let i = startValue; i <= endValue; i++)
     if(scale == 1){
-        let fahrenheit = startValue;
+        let fahrenheit = i;
         celsius = (fahrenheit - 32) * 5/9;
         document.getElementById('temperature').innerHTML += `<p>The Conversion of Fahrenheit ${fahrenheit}° to Celsius is ${celsius}°</p>`;
     }
+    for(let i = startValue; i <= endValue; i++)
     if(scale == 2){
-        let celsius = startValue;
+        let celsius = i;
         fahrenheit = (celsius * 5/9) + 32;
         document.getElementById('temperature').innerHTML += `<p>The Conversion of Fahrenheit ${celsius}° to Celsius is ${fahrenheit}°</p>`;
     }
 }
+
